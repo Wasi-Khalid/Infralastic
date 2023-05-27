5f6afc427d04dec546eba49b142ebc24a77d6d59
Wasi Khalid <wasi@bytelegions.com>
2023-05-27 16:19:02 +0500

    changes added

--- apps/app-store/src/services/store/constants/DeviceStore/index.ts
+++ apps/app-store/src/services/store/constants/DeviceStore/index.ts
@@ -1,7 +1,12 @@
-import {fetchAllCategoryList} from "../../actions/DeviceStore";
+import {fetchAllCategoryList, fetchAllProductList} from "../../actions/DeviceStore";
 
 export const AllCategoryActionType: any = {
   AllCategoryPending: [fetchAllCategoryList.pending],
   AllCategorySuccess: [fetchAllCategoryList.fulfilled],
   AllCategoryFail: [fetchAllCategoryList.rejected]
+}
+export const AllProductsActionType: any = {
+  AllProductsPending: [fetchAllProductList.pending],
+  AllProductsSuccess: [fetchAllProductList.fulfilled],
+  AllProductsFail: [fetchAllProductList.rejected]
 }

d5c251b0c9614bfef3b22a7dbd6798407211b14c
Wasi Khalid <wasi@bytelegions.com>
2023-06-03 21:01:13 +0500

    changes added

--- apps/app-store/src/services/store/state.reducer.ts
+++ apps/app-store/src/services/store/state.reducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from "redux";
 import deviceReducer from "./reducers/device.reducer";
+import orderReducer from "./reducers/order.reducer";
 
 
 const rootReducers = combineReducers({
-    device: deviceReducer,
+  device: deviceReducer,
+  order: orderReducer
 })
 
 export default rootReducers;

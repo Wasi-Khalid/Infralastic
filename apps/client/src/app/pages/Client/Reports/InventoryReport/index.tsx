da06cb50e1f7ec783c1815776ab293df15e7a6f7
Wasi Khalid <wasi@bytelegions.com>
2023-06-24 00:33:10 +0500

    reports screen api added

--- apps/client/src/app/pages/Client/Reports/InventoryReport/index.tsx
+++ apps/client/src/app/pages/Client/Reports/InventoryReport/index.tsx
@@ -5,9 +5,48 @@ import bogus from "../../../../../assets/Facebook.png";
 import {BiDotsVerticalRounded} from "react-icons/bi";
 import {BiArrowBack} from "react-icons/all";
 import {useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {fetchAssetReportByCompany, useGlobalDispatch} from "@infralastic/global-state";
+import {saveAs} from "file-saver";
 
 const InventoryReport = () => {
   const router = useNavigate();
+  const dispatch = useGlobalDispatch();
+  const [assetInfo, setAssetInfo] = useState([]);
+
+  function fetchAssetInfo() {
+    const formData = {
+      company_id: 1,
+    };
+    dispatch(fetchAssetReportByCompany(formData)).then((res: any) => {
+      setAssetInfo(res?.payload?.asset_details);
+    });
+  }
+
+  useEffect(() => {
+    fetchAssetInfo();
+  }, []);
+
+  function downloadCSV() {
+    const csvData = assetInfo.map((item) =>
+      Object.values(item).join(",")
+    );
+    const csvContent = csvData.join("\n");
+
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+    saveAs(blob, "activity_report.csv");
+  }
+  function downloadSingleCSV(id: any) {
+    // Convert assetInfo to CSV format
+    const csvData = assetInfo.filter((res: any) => res.asset_unique_id === id).map((item) =>
+      Object.values(item).join(",")
+    )
+    const csvContent = csvData.join("\n");
+
+    // Create a Blob object and trigger file download
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+    saveAs(blob, `${id}.csv`);
+  }
   return(
     <>
       <br/>
@@ -30,69 +69,135 @@ const InventoryReport = () => {
                 <input placeholder='Search' type="search" className='form-control'/>
               </div>
               <div className='mx-2'>
-                <Button variant={"secondary"}>
+                <Button
+                  variant={"secondary"}
+                  type='button'
+                  onClick={downloadCSV}
+                >
                   <AiOutlineDownload size={20} />
                 </Button>
               </div>
             </div>
           </div>
           <hr className='m-0 mt-3'/>
-          <Table striped className='theme-font' id='departmentTable'>
-            <thead className='p-3'>
-            <tr className='fs-7'>
-              <th><p className='py-2 m-0 fs-13 text-uppercase'>Item<HiChevronUpDown size={18} className='ms-1' /></p></th>
-              <th><p className='py-2 m-0 fs-13 text-uppercase'>Date<HiChevronUpDown size={18} className='ms-1' /></p></th>
-              <th><p className='py-2 m-0 fs-13 text-uppercase'>ACTION<HiChevronUpDown size={18} className='ms-1' /></p></th>
-              <th><p className='py-2 m-0 fs-13 text-uppercase'>serial Number<HiChevronUpDown size={18} className='ms-1' /></p></th>
-              <th><p className='py-2 m-0 fs-13 text-uppercase'>Assigned to<HiChevronUpDown size={18} className='ms-1' /></p></th>
-              <th><p className='py-2 m-0 fs-13'>ACTION</p></th>
+          <Table striped className="theme-font" id="departmentTable">
+            <thead className="p-3">
+            <tr className="fs-7">
+              <th>
+                <p className="py-2 m-0 fs-13 text-uppercase">
+                  Item
+                  <HiChevronUpDown size={18} className="ms-1" />
+                </p>
+              </th>
+              <th>
+                <p className="py-2 m-0 fs-13 text-uppercase">
+                  Date
+                  <HiChevronUpDown size={18} className="ms-1" />
+                </p>
+              </th>
+              <th>
+                <p className="py-2 m-0 fs-13 text-uppercase">
+                  ACTION
+                  <HiChevronUpDown size={18} className="ms-1" />
+                </p>
+              </th>
+              <th>
+                <p className="py-2 m-0 fs-13 text-uppercase">
+                  Serial Number
+                  <HiChevronUpDown size={18} className="ms-1" />
+                </p>
+              </th>
+              <th>
+                <p className="py-2 m-0 fs-13 text-uppercase">
+                  Assigned to
+                  <HiChevronUpDown size={18} className="ms-1" />
+                </p>
+              </th>
+              <th>
+                <p className="py-2 m-0 fs-13">Cost</p>
+              </th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <td>
-                <div className='d-flex align-items-center'>
-                  <img src={bogus} alt="" width='38' height='38' className='rounded' />
-                  <div className='d-flex flex-column'>
-                    <p className='m-0 ms-2 fs-7'>Lenovo</p>
+            {assetInfo?.map((item: any) => (
+              <tr key={item.id}>
+                <td>
+                  <div className="d-flex align-items-center">
+                    <img
+                      src={item?.image_url}
+                      alt=""
+                      width="38"
+                      height="38"
+                      className="rounded"
+                    />
+                    <div className="d-flex flex-column">
+                      <p className="m-0 ms-2 fs-7">{item?.asset_name}</p>
+                    </div>
                   </div>
-                </div>
-              </td>
-              <td>
-                <h6 className='text-muted fs-7 m-0'>Fri Apr 14, 2023 7:36AM</h6>
-              </td>
-              <td>
-                <h6 className='text-muted fs-7 m-0'>Checkout</h6>
-              </td>
-              <td>
-                <h6 className='text-muted fs-7 m-0'>2659952946187415</h6>
-              </td>
-              <td>
-                <div className='d-flex align-items-center'>
-                  <img src={bogus} alt="" width='32' height='32' className='rounded-circle' />
-                  <p className='m-0 ms-2 fs-7 text-muted'>Joseph Wheeler</p>
-                </div>
-              </td>
-              <td>
-                <button className='bg-success fs-7 text-white border-0 rounded px-2'>Assigned</button>
-              </td>
-              <td>
-                <div className='d-flex justify-content-end align-items-center'>
-                  <button className='bg-transparent border-0'>
-                    <AiOutlineDownload className='me-2 mt-1' size={20} />
-                  </button>
-                  <DropdownButton
-                    className="bg-transparent custom-btn"
-                    id="dropdown-item-button"
-                    title={<BiDotsVerticalRounded className='me-2' size={20} />}
-                  >
-                    <Dropdown.Item className='theme-font fs-7' as="button">Archive</Dropdown.Item>
-                    <Dropdown.Item className='theme-font fs-7' as="button">Letigation Hold</Dropdown.Item>
-                    <Dropdown.Item className='theme-font fs-7' as="button">Cyber Investigation</Dropdown.Item>
-                  </DropdownButton>
-                </div>
-              </td>
-            </tr>
+                </td>
+                <td>
+                  <h6 className="text-muted fs-7 m-0">{item?.date_added}</h6>
+                </td>
+                <td>
+                  <h6 className="text-muted fs-7 m-0">{item?.asset_tracking}</h6>
+                </td>
+                <td>
+                  <h6 className="text-muted fs-7 m-0">
+                    {item?.serial_number}
+                  </h6>
+                </td>
+                <td>
+                  {item?.employee_id ? (
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={item?.employee_image}
+                        alt=""
+                        width="32"
+                        height="32"
+                        className="rounded-circle"
+                      />
+                      <p className="m-0 ms-2 fs-7 text-muted">
+                        {item?.employee_name}
+                      </p>
+                    </div>
+                  ) : (
+                    <button className="bg-danger fs-7 text-white border-0 rounded px-2">
+                      un-assigned
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <h6 className="text-muted fs-7 m-0">{item?.cost}</h6>
+                </td>
+                <td>
+                  <div className="d-flex justify-content-end align-items-center">
+                    <button
+                      className="bg-transparent border-0"
+                      onClick={() => downloadSingleCSV(item?.asset_unique_id)}
+                    >
+                      <AiOutlineDownload className="me-2 mt-1" size={20} />
+                    </button>
+                    <DropdownButton
+                      className="bg-transparent custom-btn"
+                      id="dropdown-item-button"
+                      title={
+                        <BiDotsVerticalRounded className="me-2" size={20} />
+                      }
+                    >
+                      <Dropdown.Item className="theme-font fs-7" as="button">
+                        Archive
+                      </Dropdown.Item>
+                      <Dropdown.Item className="theme-font fs-7" as="button">
+                        Letigation Hold
+                      </Dropdown.Item>
+                      <Dropdown.Item className="theme-font fs-7" as="button">
+                        Cyber Investigation
+                      </Dropdown.Item>
+                    </DropdownButton>
+                  </div>
+                </td>
+              </tr>
+            ))}
             </tbody>
           </Table>
           <hr/>
